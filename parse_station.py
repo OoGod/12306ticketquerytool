cc6929865d8de485035e6d5b74831334c634b190
OoGod <1617605689@qq.com>
2019-03-12 23:05:09 +0800

    refresh

--- parse_station.py
+++ parse_station.py
@@ -1,7 +1,23 @@
+# coding: utf-8
+# 中文 <=> ZW 
+# 站点英文代码名字典
+
 import re
 import requests
-from pprint import pprint 
+from pprint import pprint
+
 url = 'https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.9092'
+
+# 下载数据,无需验证证书
 response = requests.get(url, verify=False)
-stations = re.findall(u'([\u4e00-\u9fa5]+)\|([A-Z]+)', response.text)
+
+# 正则匹配中文和大写字码
+stations = re.findall(r'([\u4e00-\u9fa5]+)\|([A-Z]+)', response.text)
+
+# 优雅打印
 pprint(dict(stations), indent=4)
+
+# 写入文件中
+s = dict(stations)
+with open('stations.py','w',encoding='utf-8') as f:
+    f.write(str(s))
--- tickets.py
+++ tickets.py
@@ -1,8 +1,12 @@
-# coding:utf-8
+# tickets.py
+# coding: utf-8
+
+# docopt 将文档字符串docstring中定义的格式自动解析
+# 出参数并返回一个字典
 
 """命令行火车票查看器
 Usage:
-	tickets [-gdtkz] <from> <to> <date>
+    tickets [-gdtkz] <from> <to> <date>
 """
 
 from docopt import docopt
@@ -17,11 +21,8 @@ init()
 class TrainsCollection:
     header = '车次 车站 时间 历时 一等 二等 高级软卧 软卧 硬卧 硬座 无座'.split()
 
-    def __init__(self, available_trains,available_place, options):
-        """查询的火车班次集合
-        :param available_trains: 一个列表, 包含可获得的火车班次, 每个
-                                 火车班次是一个字典
-        :param options: 查询的选项, 如高铁, 动车, etc...
+    def __init__(self, available_trains, available_place, options):
+        """查询火车班次集合
         """
         self.available_trains = available_trains
         self.available_place = available_place
@@ -29,26 +30,28 @@ class TrainsCollection:
 
     @property
     def trains(self):
+        # @propety 属性获取器装饰器,对属性的访问进行控制
         for raw_train in self.available_trains:
             raw_train_list = raw_train.split('|')
             train_no = raw_train_list[3]
             initial = train_no[0].lower()
             duration = raw_train_list[10]
             if not self.options or initial in self.options:
+                # 对始/达 着色 绿/红
                 train = [
-                    train_no,#
+                    train_no,
                     '\n'.join([Fore.LIGHTGREEN_EX + self.available_place[raw_train_list[6]] + Fore.RESET,
-                               Fore.LIGHTRED_EX + self.available_place[raw_train_list[7]] + Fore.RESET]),#始发站
+                               Fore.LIGHTRED_EX + self.available_place[raw_train_list[7]] + Fore.RESET]),
                     '\n'.join([Fore.LIGHTGREEN_EX + raw_train_list[8] + Fore.RESET,
-                               Fore.LIGHTRED_EX + raw_train_list[9] + Fore.RESET]),#终点站
-                    duration,#
-                    raw_train_list[-6] if raw_train_list[-6] else '--',# 一等 
-                    raw_train_list[-7] if raw_train_list[-7] else '--',# 二等 
-                    raw_train_list[-15] if raw_train_list[-15] else '--',# 高级软卧
-                    raw_train_list[-8] if raw_train_list[-8] else '--',#  软卧
-                    raw_train_list[-14] if raw_train_list[-14] else '--',#硬卧
-                    raw_train_list[-11] if raw_train_list[-11] else '--',#硬座
-                    raw_train_list[-9] if raw_train_list[-9] else '--',#无座
+                               Fore.LIGHTRED_EX + raw_train_list[9] + Fore.RESET]),
+                    duration,
+                    raw_train_list[-6] if raw_train_list[-6] else '--',
+                    raw_train_list[-7] if raw_train_list[-7] else '--',
+                    raw_train_list[-15] if raw_train_list[-15] else '--',
+                    raw_train_list[-8] if raw_train_list[-8] else '--',
+                    raw_train_list[-14] if raw_train_list[-14] else '--',
+                    raw_train_list[-11] if raw_train_list[-11] else '--',
+                    raw_train_list[-9] if raw_train_list[-9] else '--',
                 ]
                 yield train
 
@@ -61,24 +64,26 @@ class TrainsCollection:
 
 
 def cli():
-    """command-line interface"""
+    """ 命令行接口 """
+    # 获取文档字符串模板组成的命令行参数字典对象
     arguments = docopt(__doc__)
     from_station = stations.get(arguments['<from>'])
     to_station = stations.get(arguments['<to>'])
     date = arguments['<date>']
 
-    url = ('https://kyfw.12306.cn/otn/leftTicket/queryO?'
+    url = ('https://kyfw.12306.cn/otn/leftTicket/queryZ?'
            'leftTicketDTO.train_date={}&'
            'leftTicketDTO.from_station={}&leftTicketDTO.to_station={}&purpose_codes=ADULT').format(
-                date, from_station, to_station
-           )
+        date, from_station, to_station
+    )
+    # 不验证证书
     r = requests.get(url, verify=False)
     available_trains = r.json()['data']['result']
     available_place = r.json()['data']['map']
     options = ''.join([
         key for key, value in arguments.items() if value is True
     ])
-    TrainsCollection(available_trains,available_place, options).pretty_print()
+    TrainsCollection(available_trains, available_place, options).pretty_print()
 
 
 if __name__ == '__main__':
